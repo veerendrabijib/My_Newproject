8af85fed24327e2459bb4e0c62e7d67f5e519aff
veerendra <veerendra@bijib.in>
2026-02-18 16:35:41 +0530

    changes

--- angular/my-project17/src/frontend/src/app/components/pdf-viewer/frontend/src/app/upload-book/upload-book.component.ts/frontend/src/app/interceptors/jwt.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    return next(req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }));
-  }
-  return next(req);
-};
