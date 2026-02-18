ba8696d91a34d9afd282acd81b4296fb318624f8
veerendra <veerendra@bijib.in>
2026-02-18 14:44:56 +0530

    asas

--- node-project/backend/routes/backend/routes/backend/controllers/backend/controllers/backend/middleware/backend/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-const authorize = (...roles) => (req, res, next) => {
-  if (!roles.includes(req.user.role)) {
-    return res.status(403).json({ success: false, message: `Role '${req.user.role}' not authorized.` });
-  }
-  next();
-};
-
-module.exports = { authorize };
